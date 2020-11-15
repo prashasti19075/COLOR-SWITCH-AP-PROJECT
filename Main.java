dff129b45791ab830ed57097ab10d7e06bea7a0d
prashasti19075 <55681151+prashasti19075@users.noreply.github.com>
2020-11-15 19:19:52 +0530

    Unable to add back button from video
    Maybe its like it can't reach its(back's) event handler once we are in different scene or something

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package sample;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
@@ -15,11 +17,16 @@ import javafx.scene.control.Button;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
+import javafx.scene.media.MediaPlayer.Status;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 import javafx.scene.media.MediaPlayer;
 
 public class Main extends Application {
     //    @Override
+    Scene sc;
+    Scene videoscene;
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Colour Switch Game");
@@ -27,22 +34,21 @@ public class Main extends Application {
         Image image = new Image(new FileInputStream("src\\colorswitch2.jpg"));
         BackgroundImage myBI= new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 
-        String path = "Ipsi.mp3";
+        String path = "src/Ipsi.mp3";
         Media media = new Media(new File(path).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(media);
         mediaPlayer.play();
 
-        String videopath = "video.mp3";
-        Media video = new Media(new File(path).toURI().toString());
+        String videopath = "src/video.mp4";
+        Media video = new Media(new File(videopath).toURI().toString());
         MediaPlayer videoPlayer = new MediaPlayer(video);
         MediaView videoView = new MediaView(videoPlayer);
-        videoPlayer.play();
 
         Image titleimage = new Image(new FileInputStream("src\\title.png"));
         ImageView titleimg = new ImageView(titleimage);
         titleimg.setX(0);
         titleimg.setY(0);
-        titleimg.setFitHeight(300);
+        titleimg.setFitHeight(600);
         titleimg.setFitWidth(300);
         titleimg.setPreserveRatio(true);
         Group root = new Group(titleimg);
@@ -68,28 +74,69 @@ public class Main extends Application {
         Button quitGame = new Button("",iv3);
         quitGame.setStyle("-fx-background-color: #000000;");
 
-       // String musicFile = "Ipsi.mp3";
+        Button earnStars = new Button("Watch Video");
+        earnStars.setStyle("-fx-font-family: Algerian; -fx-text-fill: #ffffff; -fx-font-size: 20px; -fx-background-color: #000000;");
+
         VBox vBox = new VBox();
         vBox.setBackground(new Background(myBI));
         vBox.setSpacing(10);
 
-
         vBox.getChildren().add(root);
+        vBox.getChildren().add(earnStars);
         vBox.getChildren().add(newGame);
         vBox.getChildren().add(resumeGame);
         vBox.getChildren().add(quitGame);
 
-        vBox.setMargin(root, new Insets(0, 150, -250, 500));
-        vBox.setMargin(newGame, new Insets(250, 150, 20, 500));
+        vBox.setMargin(root, new Insets(0, 0, -250, 500));
+        vBox.setMargin(earnStars,new Insets(50,0,0,0));
+        vBox.setMargin(newGame, new Insets(180, 150, 20, 500));
         vBox.setMargin(resumeGame, new Insets(50, 20, 20, 500));
         vBox.setMargin(quitGame, new Insets(50, 20, 20, 500));
 
-        // create a scene
-        Scene sc = new Scene(vBox, 1200, 500);
-        primaryStage.setScene(sc);
+        double width=primaryStage.getMaxWidth();
+        double height=primaryStage.getMaxHeight();
+        sc = new Scene(vBox, width, height);
         primaryStage.setMaximized(true);
+        primaryStage.setScene(sc);
+
+        VBox newBox = new VBox();
+        Button back = new Button("Back");
+        newBox.getChildren().add(back);
+        newBox.getChildren().add(videoView);
+
+        back.setDisable(true);
+
+        videoscene = new Scene(newBox, width, height);
+
+        back.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                mediaPlayer.play();
+                primaryStage.setScene(sc);
+            }
+        });
+
+        earnStars.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent e) {
+                mediaPlayer.pause();
+                videoPlayer.play();
+                primaryStage.setScene(videoscene);
+                videoPlayer.setOnEndOfMedia(new Runnable() {
+                    @Override
+                    public void run() {
+                        back.setDisable(false);
+                        return;
+                    }
+                });
+               return;
+            }
+        });
+
 
         primaryStage.show();
+
     }
     public static void main(String[] args) {
         launch(args);
