71d145693d4cc5a67a1b7b19bc2f5ab9070856df
Neha Goel <55681416+neha19066@users.noreply.github.com>
2020-11-21 17:45:04 +0530

    Pause Controller
    1. Goes to the main menu.
    2. Opens file chooser for saving.

---
+++ sample/Pause_Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.FileChooser;
+
+import java.io.IOException;
+
+public class Pause_Controller{
+
+    @FXML
+    public void Menu() throws IOException {
+        Scene homepage = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Main.window.setScene(homepage);
+    }
+    @FXML
+    public void resume(){
+
+    }
+    @FXML
+    public void SaveandExit(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Resource File");
+        fileChooser.showOpenDialog(Main.window);
+    }
+}
